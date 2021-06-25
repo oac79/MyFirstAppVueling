39f561eca3e8c2f8f1b5c5ca58cc567a12890a5b
Oliver Abad <oabadch@gmail.com>
2021-06-25 10:48:39 +0200

    initial commit with calculator example.

---
+++ MyFirstAppVueling/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstAppVueling
+{
+    public class Calculator
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+
+    }
+}
---
+++ MyFirstAppVuelingTests/CalculatorTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyFirstAppVueling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstAppVueling.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.IsTrue(calculator.Add(2, 3) == 5);
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SubtractTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.IsTrue(calculator.Subtract(4, 2) == 2);
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MultiplyTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.IsTrue(calculator.Subtract(4, 2) == 8);
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DivideTest()
+        {
+            Calculator calculator = new Calculator();
+            Assert.IsTrue(calculator.Subtract(4, 2) == 2);
+            //Assert.Fail();
+        }
+    }
+}
